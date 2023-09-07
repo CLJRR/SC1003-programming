b9712796df1f463e895db04dc8419b9c5653eb01
CLJR <calvinleejiarong@gmail.com>
2023-09-07 16:42:25 +0800

    final

--- Week 3/Ex3.py
+++ Week 3/Ex3.py
@@ -1,54 +1,77 @@
+# dictionary of existing users
 userDict = {
     'Test': 'Test12345',
     'Jack': 'Test12345',
     'Tom': 'Password1',
 }
+
+
+# function to check if username is in existing users
+# return 1 if username is taken
+# retuen 0 if username is free
 def usernameChecker(Username, userList):
-    check = False
-    while check != True:
-        if Username in list(userList):
-            return 0
-        else:
-            return 1
-
-
-def passwordchecker(Password):
-    upper = lower = digit = int(0)
-    password = Password
-    length = int(len(password))
-    if length == 0:
+    if Username in list(userList):
+        return 1
+    else:
         return 0
+
+
+# function to check if password meets req
+# ask for the User's Input to be shared through variable "Password"
+# returns 1 if password does not meet requirement
+# returns 0 if password meets requirements
+def passwordchecker(Password):
+    # set variables to false/0
+    upper = lower = digit = int(0)
+    length = int(len(Password))
+    if length <= 0:
+        return 1
+    # this is prob how u will do it in C language
     for i in range(0, length):
-        if password[i].isupper() == 1:
+        if Password[i].isupper() == 1:
             upper = 1
-        if password[i].islower() == 1:
+        if Password[i].islower() == 1:
             lower = 1
-        if password[i].isdigit() == 1:
+        if Password[i].isdigit() == 1:
             digit = 1
         if upper*lower*digit == 1:
-            return 1
+            return 0
+    return 1
 
 
 def main():
-
     check = False
+    # To get username
     while check != True:
+        # req UserInput
         Username = input("User registration:\nInput your user name:")
-        if usernameChecker(Username, userDict) == 1:
+        # calls Function usernameChecker, and shares userInput variables "Username" and GLOBAL variable "userDict"
+        # if function returns 0 (username is free)
+        if usernameChecker(Username, userDict) == 0:
             break
+        # else if function returns 1 (username is taken)
         else:
             print("The user exists. Please choose another user name.")
 
+    # To get password
     while check != True:
+        # req UserInput
         Password = input(
             "Input your password:\n1.the password has at least one upper case letter\n2.the password has at least one lower case letter\n3.the password has at least one digit\n")
-        if passwordchecker(Password) == 1:
+        # Calls Function passwordchecker, and shares UserInput Variable "Password"
+        # if funct returns 0 (password meets requirements)
+        if passwordchecker(Password) == 0:
             print("Your password is strong enough. User registered.")
             break
         else:
             print("Your password is weak. Please enter a new password")
+    # At this pt, username and password are both valid
+
+    # add username and password to dictionary
     userDict[Username] = Password
+    # print all existing users in dictionary
     print(userDict)
 
 
+# this main() is to tell vscode / idle that this is the main program to run
 main()
--- Week 3/Test2.py
+++ Week 3/Test2.py
@@ -1,32 +1,3 @@
 
-
-usernames = {
-    0: "Test",
-    1: "Jack",
-    2: "Tom",
-}
-passwords = {
-    0: 'Test12345',
-    1: 'Test12345',
-    2: 'Password1',
-}
-check = False
-userCheck = False
-# while check != True:
-#     Username = "Tom"
-#     for i in range(0, 3, 1):
-#         if Username is usernames[i]:
-#             userCheck = True
-#         print()
-#         print(Username, usernames[i], userCheck)
-#     break
-
-while check != True:
-    Username = input("User registration:\nInput your user name:")
-    # Username = "Tom"
-    for i in range(0, 3, 1):
-        if Username is usernames[i]:
-            userCheck = True
-        print()
-        print(Username, usernames[i], userCheck)
-    break
+list1 = [x ** 2 for x in range(1, 101, 1) if x ** 2 % 3 == 0]
+print(list1)
