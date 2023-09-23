82193ca7bc71ac0c002c847010af760385963af5
CLJRR <calvinleejiarong@gmail.com>
2023-09-24 01:09:18 +0800

    f

--- revision python/test.py
+++ revision python/test.py
@@ -1,15 +1,28 @@
+# def sumOfNodes(t):
+#     if len(t) == 1:
+#         return t[0]
+#     else:
+#         left = sumOfNodes(t[0])
+#         right = sumOfNodes(t[2])
+#         max = t[1]
+#         if (right < max):
+#             max = right
+#         if (left < max):
+#             max = left
+
+#     return (max)
 def sumOfNodes(t):
     if len(t) == 1:
-        return 1
+        return [t[0]]
     else:
         left = sumOfNodes(t[0])
         right = sumOfNodes(t[2])
 
-    return (left+right+1)
+    return [right, t[1], left]
 
 
 def main():
-    tree = [[[7], 2, [9]], 3, [[8], 2, [4]]]
+    tree = [[[7], 1, [9]], 3, [[8], 2, [4]]]
     # print(tree)
     count = sumOfNodes(tree)
     print(count)
