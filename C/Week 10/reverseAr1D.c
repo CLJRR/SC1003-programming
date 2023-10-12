52e84da008d1d3ac1cc047f047195734b8dbb07a
CLJR <calvinleejiarong@gmail.com>
2023-10-13 01:06:08 +0800

    v1

---
+++ C/Week 10/reverseAr1D.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+void reverseAr1D(int ar[], int size);
+int main()
+{
+    int ar[10];
+    int size, i;
+
+    printf("Enter array size: \n");
+    scanf("%d", &size);
+
+    printf("Enter %d data: \n", size);
+    for (i = 0; i <= size - 1; i++)
+        scanf("%d", &ar[i]);
+
+    reverseAr1D(ar, size);
+    printf("reverseAr1D(): ");
+    if (size > 0)
+    {
+        for (i = 0; i < size; i++)
+            printf("%d ", ar[i]);
+    }
+    return 0;
+}
+void reverseAr1D(int ar[], int size)
+{
+    int *p = &ar[0];
+    int outArray[size];
+    for (int i = 0, j = size - 1; i < size; i++, j--)
+        outArray[i] = ar[j];
+    for (int i = 0; i < size; i++)
+        *p = outArray[i];
+    // outArray[0] = ar[j];
+    // for (int i = 0, j = size; i < size; i++, j--)
+    //     printf("%d\n", outArray[i]);
+
+    ar[size] = outArray[size];
+}
