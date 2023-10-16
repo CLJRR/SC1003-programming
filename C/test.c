f475b44baf395a07761773f5b7842c2aed631b9e
CLJR <calvinleejiarong@gmail.com>
2023-10-16 22:01:40 +0800

    ff

---
+++ C/test.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int height, width;
+    printf("Enter Height and Width:");
+    scanf("%d %d", &height, &width);
+    for (int i = 0; i < width; i++)
+    {
+        printf("=");
+    }
+    printf("\n");
+    for (int i = 0; i < height - 2; i++)
+    {
+        printf("*");
+        for (int j = 0; j < width - 2; j++)
+        {
+            printf(" ");
+        }
+        printf("*\n");
+    }
+    for (int i = 0; i < width; i++)
+    {
+        printf("=");
+    }
+    printf("\n");
+    // }
+
+    // int main(void)
+    // {
+    //     int height;
+    //     printf("Please enter height");
+    //     scanf("%d", &height);
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d", i);
+        }
+        printf("\n");
+    }
+    // }
+    // int main(void)
+    // {
+    // int height;
+    // printf("Please enter height: ");
+    // scanf("%d", &height);
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = 1; j <= height - i; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 0; k < i; k++)
+        {
+            printf("%d", i);
+        }
+        printf("\n");
+    }
+}
